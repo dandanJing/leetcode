a396cdbc518c4f89063108c8d7035f90b6a6fbb1
jing <1544299536@qq.com>
2015-10-11 10:51:13 +0800

    将287题改成非迭代版本

--- leetcode/leetcode/leetcode/find_duplicate_287.cpp
+++ leetcode/leetcode/leetcode/find_duplicate_287.cpp
@@ -5,7 +5,27 @@ int findDuplicateOp(vector<int>& nums,int div_small,int div_middle,int div_large
 class Solution{
 public:
 	int findDuplicate(vector<int>& nums){
-		return findDuplicateOp(nums,1,nums.size()/2,nums.size()-1);
+		int div_small=1,div_middle=nums.size()/2,div_large=nums.size()-1;
+		while(1){
+			if(div_small==div_large) return div_small;
+			else if (div_small>div_large) return -1;
+
+			int small_count=0,large_count=0;
+			FOR(i,nums.size()){
+				int temp=nums[i];
+				if(temp>=div_small && temp<=div_middle)small_count++;
+				else if(temp>div_middle && temp<=div_large)large_count++;
+			}
+
+			if(small_count>div_middle-div_small+1) {
+				div_large=div_middle;
+				div_middle=(div_middle+div_small)/2;
+			}else{
+				div_small=div_middle+1;
+				div_middle=(div_middle+1+div_large)/2;
+			}
+		}
+		//return findDuplicateOp(nums,1,nums.size()/2,nums.size()-1);
 	}
 };
 
