4cd1cd37884c6e2daef0ecb98ea59b9c1f6c70fe
= <=>
2015-10-12 12:31:36 +0800

    解决merge冲突

---
+++ leetcode/leetcode_new/leetcode_new/main.cpp
@@ -0,0 +1,18 @@
+#include"top.h"
+using namespace std;
+
+void let_290();
+void let_289();
+void let_287();
+void let_284();
+
+int main(){
+	//let_290();
+	//let_287();
+	//let_289();
+	let_284();
+
+	printf("press any key to continue....");
+	getchar();
+	return 0;
+}
