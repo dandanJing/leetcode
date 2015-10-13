4ccb7f29997dfe42645c60eb65b0f1201559d837
= <=>
2015-10-13 17:49:07 +0800

    完成241题

--- leetcode/leetcode_new/leetcode_new/different_ways_241.cpp
+++ leetcode/leetcode_new/leetcode_new/different_ways_241.cpp
@@ -2,18 +2,60 @@
 
 //-----!!!错误1：输入"2-1-1-1-1"，期望输出[-2,0,0,0,0,0,0,2,2,2,2,2,2,4]，实际输出[-2,0,0,2,0,0,2,2,4,2,2,0,2]
 void findDiffResult(string input,int cur_pos,int temp_value,vector<int> &result,bool hasPre,int preVal,char preOp);
+vector<int> findResult(vector<char>op_vec,vector<int>num_vec,int start_pos,int end_pos);
+
 vector<int> Solution::diffWaysToCompute(string input) {
 	vector<int> result;
 	if(input.size()==0)return result;
 
-	int pos=0;
+	vector<char>op_vec;
+	vector<int>num_vec;
+	int temp_value=0;
+	FOR(i,input.size()){
+		if(input[i]<='9' && input[i]>='0')temp_value=temp_value*10+input[i]-'0';
+		else{op_vec.push_back(input[i]);num_vec.push_back(temp_value);temp_value=0;}
+	}
+	num_vec.push_back(temp_value);
+
+	result=findResult(op_vec,num_vec,0,num_vec.size()-1);
+	/*int pos=0;
 	int temp_value=0;
 	while(pos<input.size()){
 		if(input[pos]<='9' && input[pos]>='0')temp_value=temp_value*10+input[pos++]-'0';
 		else break;
 	}
 
-	findDiffResult(input,pos,temp_value,result,0,0,' ');
+	findDiffResult(input,pos,temp_value,result,0,0,' ');*/
+	return result;
+}
+
+int calcuOp(int val1,int val2,char op_char){
+	int op_result=0;
+	if(op_char=='+')op_result=val1+val2;
+	else if(op_char=='-')op_result=val1-val2;
+	else op_result=val1*val2;
+
+	return op_result;
+}
+
+vector<int> findResult(vector<char>op_vec,vector<int>num_vec,int start_pos,int end_pos){
+	vector<int> result;
+	if(start_pos>end_pos) return result;
+	else if(start_pos==end_pos) {result.push_back(num_vec[start_pos]);return result;}
+	else if(start_pos+1==end_pos) {
+		int temp=calcuOp(num_vec[start_pos],num_vec[end_pos],op_vec[start_pos]);
+		result.push_back(temp);return result;}
+
+	for(int i=start_pos;i<end_pos;i++){
+		vector<int> temp1=findResult(op_vec,num_vec,start_pos,i);
+		vector<int> temp2=findResult(op_vec,num_vec,i+1,end_pos);
+		FOR(j,temp1.size()){
+			FOR(k,temp2.size()){
+				int temp=calcuOp(temp1[j],temp2[k],op_vec[i]);
+				result.push_back(temp);
+			}
+		}
+	}
 	return result;
 }
 
