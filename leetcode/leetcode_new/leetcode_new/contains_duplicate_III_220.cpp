54660eb8e3a632ff94d7b67b63585ec2efe2eba2
= <=>
2015-10-15 20:42:11 +0800

    完成220题，但是需要提高速率

--- leetcode/leetcode_new/leetcode_new/contains_duplicate_III_220.cpp
+++ leetcode/leetcode_new/leetcode_new/contains_duplicate_III_220.cpp
@@ -1,6 +1,25 @@
 #include"top.h"
 
 bool Solution::containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t){
+	int *nums_array=(int *) new int[k+1];
+	
+	int temp_size=k+1;
+	if(nums.size()<k+1)temp_size=nums.size();
+
+	FOR(i,temp_size){
+		FOR(j,i-1){
+			if(abs(nums_array[j]-nums[i])<=t) return true;
+		}
+		nums_array[i]=nums[i];
+	}
+
+	for(int i=k+1;i<nums.size();i++){
+		for(int j=1;j<k+1;j++){
+			if(abs(nums_array[j]-nums[i])<=t)return true;
+			else nums_array[j-1]=nums_array[j];
+		}
+		nums_array[k]=nums[i];
+	}
 	return false;
 }
 
@@ -15,5 +34,5 @@ void let_220(){
 		nums.push_back(value);
 	}
 	Solution sol;
-	cout<<sol.containsNearbyAlmostDuplicate(nums,k,1)<<endl;;
+	cout<<sol.containsNearbyAlmostDuplicate(nums,k,2)<<endl;;
 }
