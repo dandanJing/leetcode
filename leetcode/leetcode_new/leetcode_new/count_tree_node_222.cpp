4c3817c3e28afe6f850c4ab58bcbdbb02313838c
= <=>
2015-10-15 19:23:45 +0800

    完成222题

--- leetcode/leetcode_new/leetcode_new/count_tree_node_222.cpp
+++ leetcode/leetcode_new/leetcode_new/count_tree_node_222.cpp
@@ -1,35 +1,55 @@
 #include"top.h"
 
-int countTreeNode(TreeNode* root);
-int Solution::countNodes(TreeNode* root){
-	return countTreeNode(root);
+//----!!!错误1：要充分利用满二叉树的性质，提高时间效率
+//----!!!错误2：输入[1,2,3,4,5,6,7,8]，期望8，输出7
+int findTreeHeight(TreeNode* root){
+	TreeNode* temp=root;
+	int height=0;
+	while(temp){
+		height++;
+		temp=temp->left;
+	}
+	return height;
 }
 
-int countTreeNode(TreeNode* root){
-	int result=1;
+int Solution::countNodes(TreeNode* root){
 	if(root==NULL) return 0;
-	else{
-		if(root->left) result+=countTreeNode(root->left);
-		if(root->right) result+=countTreeNode(root->right);
+
+	int result=1;
+	TreeNode* temp=root;
+	while(temp){
+		int rightHeight=findTreeHeight(temp->right);
+		int leftHeight=findTreeHeight(temp->left);
+		if(rightHeight>=leftHeight && rightHeight){
+			result=2*result+1;
+			temp=temp->right;
+		}else if(leftHeight){
+			result=2*result;
+			temp=temp->left;
+		}else{
+			temp=temp->left;
+		}
 	}
 	return result;
 }
 
 void let_222(){
-	TreeNode* tree=(TreeNode*)(new TreeNode(4));
+	TreeNode* tree=(TreeNode*)(new TreeNode(1));
 	TreeNode* node1=(TreeNode*)(new TreeNode(2));
-	TreeNode* node2=(TreeNode*)(new TreeNode(7));
+	TreeNode* node2=(TreeNode*)(new TreeNode(3));
 	tree->left=node1;
 	tree->right=node2;
-	TreeNode* node3=(TreeNode*)(new TreeNode(1));
-	TreeNode* node4=(TreeNode*)(new TreeNode(3));
+	TreeNode* node3=(TreeNode*)(new TreeNode(4));
+	TreeNode* node4=(TreeNode*)(new TreeNode(5));
 	node1->left=node3;
 	node1->right=node4;
 	TreeNode* node5=(TreeNode*)(new TreeNode(6));
-	TreeNode* node6=(TreeNode*)(new TreeNode(9));
+	TreeNode* node6=(TreeNode*)(new TreeNode(7));
 	node2->left=node5;
 	node2->right=node6;
-	
+	TreeNode* node7=(TreeNode*)(new TreeNode(8));
+	/*node3->left=node7;*/
+
 	Solution sol;
 	cout<<sol.countNodes(tree)<<endl;
 }
