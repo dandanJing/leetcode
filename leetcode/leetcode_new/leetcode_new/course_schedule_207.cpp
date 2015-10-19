184141f53e50994623a2b95d2bf46213d91e8ca2
= <=>
2015-10-19 10:56:46 +0800

    考虑非迭代版本，算法有误

--- leetcode/leetcode_new/leetcode_new/course_schedule_207.cpp
+++ leetcode/leetcode_new/leetcode_new/course_schedule_207.cpp
@@ -1,19 +1,21 @@
 #include"top.h"
+extern char c[1000];
 
-bool canFindCicle(vector<bool>& exist_vec,vector<vector<int>> list,int start_pos, vector<int> contain){
+bool canFindCicle(vector<bool>& exist_vec,vector<vector<int>> list,int start_pos, map<int,int> contain){
 	if(start_pos>=list.size()) return false;
-	if(exist_vec[start_pos]) {vector<int> no_vec;return canFindCicle(exist_vec,list,start_pos++,no_vec);}
+	if(exist_vec[start_pos]) {map<int,int> no_vec;return canFindCicle(exist_vec,list,start_pos++,no_vec);}
 
-	FOR(i,contain.size()){
-		if(start_pos==contain[i]) return true;
-	}
+	map<int,int>::iterator it;
+	it=contain.find(start_pos);
+	if(it!=contain.end()) return true;
+	
 	vector<int> temp=list[start_pos];
 	FOR(i,temp.size()){
 		int cur_pos=temp[i];
 		if(exist_vec[cur_pos])continue;
 		else{
-			vector<int> no_vec(contain);
-			no_vec.push_back(start_pos);
+			map<int,int> no_vec(contain);
+			no_vec.insert(pair<int,int>(start_pos,1));
 			if(canFindCicle(exist_vec,list,cur_pos,no_vec)) return true;
 		}
 	}
@@ -23,8 +25,11 @@ bool canFindCicle(vector<bool>& exist_vec,vector<vector<int>> list,int start_pos
 }
 
 bool Solution::canFinish(int numCourses, vector<pair<int, int>>& prerequisites){
+	if(prerequisites.size()<=1) return true;
+
 	vector<vector<int>> list;
 	vector<bool> exist_vec;
+	int min_val=numCourses;
 	FOR(i,numCourses){
 		exist_vec.push_back(false);
 		vector<int> temp;
@@ -33,14 +38,61 @@ bool Solution::canFinish(int numCourses, vector<pair<int, int>>& prerequisites){
 	FOR(i,prerequisites.size()){
 		pair<int, int> cur=prerequisites[i];
 		list[cur.first].push_back(cur.second);
+		if(cur.first<min_val) min_val=cur.first;
 	}
-	FOR(i,numCourses){
+	queue<int> exist_queue;
+	map<int,int> contain;
+	map<int,int>::iterator it;
+	for(int i=min_val;i<numCourses;i++){
+		if(i==605){
+			int mm=1;
+		}
 		if(exist_vec[i]) continue;
-		vector<int> no_vec;
-		if(canFindCicle(exist_vec,list,i,no_vec)) return true;
+		vector<int> temp=list[i];
+
+		if(temp.size()==0) continue;
+
+		FOR(j,temp.size())exist_queue.push(temp[j]);
+		exist_vec[i]=true;
+		sprintf(c,"%i",i);
+		string output="";
+		output += c;
+		
+		contain.insert(pair<int,int>(i,1));
+
+		while(1){
+			vector<int> next_queue;
+			vector<int> next_contain;
+			while(exist_queue.size()){
+				vector<int> temp=list[exist_queue.front()];
+				FOR(j,temp.size()){
+					if(contain.find(temp[j]) != contain.end()) {
+						cout<<output<<endl;
+						return false;
+					}
+					else {next_queue.push_back(temp[j]);}
+				}
+				exist_vec[exist_queue.front()]=true;
+				next_contain.push_back(exist_queue.front());
+				exist_queue.pop();
+			}
+			if(next_queue.size()==0) break;
+
+			output += "->";
+			FOR(j,next_contain.size()){
+				sprintf(c,"%i ",next_contain[j]);
+				output += c;
+				contain.insert(pair<int,int>(next_contain[j],1));
+			}
+
+			FOR(j,next_queue.size()){
+				exist_queue.push(next_queue[j]);
+			}		
+		}
+		contain.clear();
 	}
 	
-	return false;
+	return true;
 }
 
 void let_207(){
