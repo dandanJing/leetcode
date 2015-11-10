42b87c7124260df0ae2dd97c89f2aa781c0e38f7
jing <1544299536@qq.com>
2015-11-10 21:22:14 +0800

    完成100题

--- leetcode/leetcode_new/leetcode_new/same_tree_100.cpp
+++ leetcode/leetcode_new/leetcode_new/same_tree_100.cpp
@@ -1,6 +1,25 @@
 #include"top.h"
 
-bool isSameTree(TreeNode* p, TreeNode* q){
+bool Solution::isSameTree(TreeNode* p, TreeNode* q){
+	queue<TreeNode*>queue1;
+	queue<TreeNode*>queue2;
+	queue1.push(p);
+	queue2.push(q);
+	TreeNode* temp1,*temp2;
+	while(queue1.size()){
+		temp1=queue1.front();
+		queue1.pop();
+		temp2=queue2.front();
+		queue2.pop();
+		if(temp1 && temp2){
+			if(temp1->val!=temp2->val)return false;
+			queue1.push(temp1->left);
+			queue1.push(temp1->right);
+			queue2.push(temp2->left);
+			queue2.push(temp2->right);
+		}else if(!temp1 && !temp2)continue;
+		else return false;
+	}
 	return true;
 }
 
@@ -16,7 +35,7 @@ void let_100(){
 	node2->right=node4;
 	TreeNode* temp=(TreeNode*)new TreeNode(3);
 	temp->left=node1;
-	temp->right=node2;
+	//temp->right=node2;
 	Solution sol;
 	cout<<sol.isSameTree(root,temp)<<endl;
 }
