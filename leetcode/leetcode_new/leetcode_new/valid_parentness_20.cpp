40105ada2a84a26cc51ae0668b3ceddfdc1dfccb
= <=>
2015-11-12 10:37:15 +0800

    完成20题

--- leetcode/leetcode_new/leetcode_new/valid_parentness_20.cpp
+++ leetcode/leetcode_new/leetcode_new/valid_parentness_20.cpp
@@ -1,8 +1,27 @@
 #include"top.h"
 
 bool Solution::isValid(string s){
+	vector<char>char_vec;
+	FOR(i,s.size()){
+		if(s[i]==' ')continue;
+		if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+			char_vec.push_back(s[i]);
+		}else if(char_vec.size()){
+			char temp=char_vec.back();
+			char_vec.pop_back();
+			if(temp=='[' && s[i]==']')continue;
+			if(temp=='(' && s[i]==')')continue;
+			if(temp=='{' && s[i]=='}')continue;
+			return false;
+		}else{
+			return false;
+		}
+	}
+	if(char_vec.size()) return false;
 	return true;
 }
 
 void let_20(){
+	Solution sol;
+	cout<<sol.isValid("()]{}")<<endl;
 }
