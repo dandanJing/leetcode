220f21e11cd47ee3d6422fd9575255aa73edae4a
= <=>
2015-11-19 17:20:22 +0800

    完成80题

--- leetcode/leetcode_new/leetcode_new/remove_duplicates_sorted_array_80.cpp
+++ leetcode/leetcode_new/leetcode_new/remove_duplicates_sorted_array_80.cpp
@@ -1,8 +1,37 @@
 #include"top.h"
 
 int Solution::removeDuplicates(vector<int>& nums){
-	return 5;
+	int cur=0,temp;
+	int count=0;
+	while(cur<nums.size()){
+		temp=cur+1;
+		while(temp<nums.size()){
+			if(nums[temp]==nums[cur])temp++;
+			else break;
+		}
+		if(temp-cur>=2){
+			nums[count++]=nums[cur];
+			nums[count++]=nums[cur+1];
+		}else{
+			nums[count++]=nums[cur];
+		}
+		cur=temp;
+	}
+	return count;
 }
 
 void let_80(){
+	ifstream fin;
+	fin.open("let_80.txt");
+	char tempc=' ';
+	int value;
+	vector<int> nums;
+	while(fin.peek()!=']'){
+		fin>>tempc>>value;
+		nums.push_back(value);
+	}
+	Solution sol;
+	cout<<sol.removeDuplicates(nums)<<endl;
+	FOR(i,nums.size())cout<<nums[i]<<",";
+	cout<<endl;
 }
