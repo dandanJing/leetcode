5c62efeb6a56fa6bec6e444b4b5ec83acd1c43db
jing <1544299536@qq.com>
2015-11-22 09:49:43 +0800

    完成第9题

--- leetcode/leetcode_new/leetcode_new/palindrome_number_9.cpp
+++ leetcode/leetcode_new/leetcode_new/palindrome_number_9.cpp
@@ -1,8 +1,35 @@
 #include"top.h"
 
+//错误1---输入1，期望输出true
+//错误2---输入-2147483648，期望输出false
+//错误3---输入-3,期望输出false
 bool Solution::isPalindrome(int x) {
+	if(x==0x80000000)return false;
+	if(x<0)return false;
+	if(x<10)return true;
+	
+	int basemin=1,basemax=10;
+	int temp=x/basemax;
+	while(temp>=10){
+		basemax*=10;
+		temp=x/basemax;
+	}
+	int val1,val2;
+	while(basemax>basemin){
+		val1=(x/basemax)%10;
+		val2=(x/basemin)%10;
+		if(val1!=val2)return false;
+
+		basemin*=10;
+		basemax/=10;
+	}
 	return true;
 }
 
 void let_9(){
+	Solution sol;
+	cout<<sol.isPalindrome(-2147483648)<<endl;
+	cout<<sol.isPalindrome(-321)<<endl;
+	cout<<sol.isPalindrome(100)<<endl;
+	cout<<sol.isPalindrome(12321)<<endl;
 }
